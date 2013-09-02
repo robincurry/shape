72604e3e6d1fcb08b090275f594298b2efa3126e
Robin Curry <robin.curry@vitals.com>
2013-09-02 14:58:18 -0500

    Make it easier to include.

--- lib/shaper.rb
+++ lib/shaper.rb
@@ -3,3 +3,10 @@ require 'shaper/base'
 require 'shaper/property_shaper'
 require 'shaper/association_shaper'
 require 'shaper/data_visitor'
+require 'shaper/renderers'
+
+module Shaper
+  extend ActiveSupport::Concern
+  include Shaper::Base
+  include Shaper::Renderers
+end
