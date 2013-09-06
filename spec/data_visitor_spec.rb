35486b0acb20aa022ab919e378aee3bed72b45b5
Brandon Westcott <brandon.westcott@vitals.com>
2013-09-06 13:10:01 -0500

    Fix data visitor for each_with relations

--- spec/data_visitor_spec.rb
+++ spec/data_visitor_spec.rb
@@ -5,51 +5,105 @@ describe Shape::DataVisitor do
   context 'Given a decorated class that implements Shape::DataVisitor' do
 
     before do
-      stub_const('PersonDecorator', Class.new do
-        include Shape::Base
-        include Shape::DataVisitor
+      stub_const('FamilyMember', Class.new do
+        include Shape
         property :name
         property :age
-        association :spouse, with: self
+      end)
+
+      stub_const('ParentDecorator', Class.new do
+        include Shape
+        property :name
+        property :age
+        association :spouse, with: FamilyMember
+        association :children, each_with: FamilyMember
       end)
     end
 
-    let(:source) {
-      OpenStruct.new.tap do |person|
-        person.name  = 'John Smith'
-        person.age   = 34
-        person.spouse = OpenStruct.new.tap do |spouse|
-          spouse.name  = 'Jane Smith'
-          spouse.age = 32
+    context 'when I visit an object with relations' do
+      let(:source) {
+        OpenStruct.new.tap do |person|
+          person.name  = 'John Smith'
+          person.age   = 34
+          person.spouse = OpenStruct.new.tap do |spouse|
+            spouse.name  = 'Jane Smith'
+            spouse.age = 32
+          end
+          person.children = [
+            OpenStruct.new.tap do |spouse|
+              spouse.name  = 'Sally Smith'
+              spouse.age = 5
+            end
+          ]
+        end
+      }
+
+      context 'without providing a visitor' do
+        subject {
+          ParentDecorator.new(source).visit
+        }
+
+        it 'returns the raw visited data' do
+          expect(subject[:name]).to eq('John Smith')
+          expect(subject[:age]).to eq(34)
+          expect(subject[:spouse][:name]).to eq('Jane Smith')
+          expect(subject[:spouse][:age]).to eq(32)
+          expect(subject[:children].first[:name]).to eq('Sally Smith')
+          expect(subject[:children].first[:age]).to eq(5)
         end
       end
-    }
 
-    context 'when I visit the object without providing a visitor' do
-      subject {
-        PersonDecorator.new(source).visit
-      }
+      context 'with a string visitor' do
+        subject {
+          ParentDecorator.new(source).visit(lambda do |data|
+            data.to_s
+          end)
+        }
 
-      it 'returns the raw visited data' do
-        expect(subject[:name]).to eq('John Smith')
-        expect(subject[:age]).to eq(34)
-        expect(subject[:spouse][:name]).to eq('Jane Smith')
-        expect(subject[:spouse][:age]).to eq(32)
+        it 'returns the visited data as strings' do
+          expect(subject[:name]).to eq('John Smith')
+          expect(subject[:age]).to eq('34')
+          expect(subject[:spouse][:age]).to eq('32')
+          expect(subject[:children].first[:age]).to eq('5')
+        end
       end
+
     end
 
-    context 'when I visit the object with a string visitor' do
-      subject {
-        PersonDecorator.new(source).visit(lambda do |data|
-          data.to_s
-        end)
+    context 'when I visit an object with empty relations' do
+      let(:source) {
+        OpenStruct.new.tap do |person|
+          person.name  = 'John Smith'
+          person.age   = 34
+          person.spouse = nil
+          person.children = []
+        end
       }
 
-      it 'returns the visited data as strings' do
-        expect(subject[:name]).to eq('John Smith')
-        expect(subject[:age]).to eq('34')
-        expect(subject[:spouse][:age]).to eq('32')
+      context 'without providing a visitor' do
+        subject {
+          ParentDecorator.new(source).visit
+        }
+
+        it 'returns the raw visited data' do
+          expect(subject[:spouse]).to eq(nil)
+          expect(subject[:children]).to eq([])
+        end
       end
+
+      context 'with a string visitor' do
+        subject {
+          ParentDecorator.new(source).visit(lambda do |data|
+            data.to_s
+          end)
+        }
+
+        it 'returns the raw visited data' do
+          expect(subject[:spouse]).to eq(nil)
+          expect(subject[:children]).to eq([])
+        end
+      end
+
     end
 
   end
